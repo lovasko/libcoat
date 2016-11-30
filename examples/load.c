09b93c7acd28fc6b83986f297b2e544bf438cb57
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-11-30 13:31:05 +0000

    magic constant naming; indentation resolution

--- examples/load.c
+++ examples/load.c
@@ -2,19 +2,25 @@
 #include <unistd.h>
 #include <coat.h>
 
+#define WAIT_TIME 60
+
+/**
+  * Query the OS for the system load average over the last minute and
+  * store the values in a GoatSwim raw format.
+**/
 int
 main(void)
 {
   coat c;
-	double avg;
+  double avg;
 
   coat_open(&c, "load.gs", NULL, 0);
 
   while (1) {
-	  getloadavg(&avg, 1);
+    getloadavg(&avg, 1);
     coat_write(&c, (float)avg);
-		sleep(60);
+    sleep(WAIT_TIME);
   }
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
