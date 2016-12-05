9ade3b76435dff76591a879f7bce9f42e727e174
Daniel Lovasko <daniel.lovasko@gmail.com>
2016-12-05 14:44:21 +0000

    proper error handling in load example

--- examples/load.c
+++ examples/load.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <coat.h>
 
 #define WAIT_TIME 60
+
+static void
+err(int ret, char* name)
+{
+  if (ret == COAT_E_NULL)
+    fprintf(stderr, "%s: null error", name);
+
+  if (ret == COAT_E_IO)
+    perror(name);
+}
 
 /**
   * Query the OS for the system load average over the last minute and
@@ -13,13 +24,30 @@ main(void)
 {
   coat c;
   double avg;
+  int ret;
 
-  coat_open_file(&c, "load.gs", NULL, 0);
+  ret = coat_open_file(&c, "load.gs", NULL, 0);
+  if (ret != COAT_OK) {
+    err(ret, "open_file");
+    return EXIT_FAILURE;
+  }
 
   while (1) {
     getloadavg(&avg, 1);
-    coat_write(&c, (float)avg);
+    printf("%f\n", (float)avg);
+    ret = coat_write(&c, (float)avg);
+    if (ret != COAT_OK) {
+      err(ret, "write");
+      break;
+    }
+
     sleep(WAIT_TIME);
+  }
+
+  ret = coat_close(&c);
+  if (ret != COAT_OK) {
+    err(ret, "close");
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
